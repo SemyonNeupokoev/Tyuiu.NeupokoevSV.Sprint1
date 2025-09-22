2e21e5966b52417fe65ff5ec09082bb0ab626c69
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-09-22 09:41:11 +0300

    Add project files.

---
+++ Tyuiu.NeupokoevSV.Sprint1.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.NeupokoevSV.Sprint1.Task0.V25.Lib
+{
+    public class DataService : ISprint1Task0V25
+    {
+        public double Calculate()
+        {
+            return (30 / 6 - 4) * 3;
+        }
+    }
+}
+
---
+++ Tyuiu.NeupokoevSV.Sprint1.Task0.V25/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.NeupokoevSV.Sprint1.Task0.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Неупокоев С. В.| ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнил: Неупокоев С. В.| ПИНб-25-1                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение (30 / 6 - 4) * 3        *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* (30 / 6 - 4) * 3                                                        *");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}

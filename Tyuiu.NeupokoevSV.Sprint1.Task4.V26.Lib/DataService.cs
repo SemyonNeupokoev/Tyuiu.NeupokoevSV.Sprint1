92a088c868e9c337e8c3bfce62f6723339ef8133
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-09-27 12:50:23 +0300

    Task4

---
+++ Tyuiu.NeupokoevSV.Sprint1.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.NeupokoevSV.Sprint1.Task4.V26.Lib
+{
+    public class DataService : ISprint1Task4V26
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = (Math.Atan(x) + y) / Math.Exp(y + x);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint1.Task4.V26/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.NeupokoevSV.Sprint1.Task4.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1  | Выполнил: Неупокоев С. В.| ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #26                                                              *");
+        Console.WriteLine("* Выполнил: Неупокоев С. В.| ПИНб-25-1                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+        Console.WriteLine("* одинаковых массивов по длинне.                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine();
+        int x;
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToInt32(Console.ReadLine());
+        int y;
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.ReadKey();
+    }
+}

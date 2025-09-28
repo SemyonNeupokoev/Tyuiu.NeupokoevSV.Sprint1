6d165916fb1e20aa5953c0e76cc718d8898e5a9b
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-09-28 15:09:50 +0300

    Task7

---
+++ Tyuiu.NeupokoevSV.Sprint1.Task7.V1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.NeupokoevSV.Sprint1.Task7.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine();
+        Console.WriteLine("Введите значение A:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение B:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение C:");
+        double c = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double z = ds.Calculate(a, b, c);
+        Console.WriteLine(z);
+        Console.ReadKey();
+    }
+}
